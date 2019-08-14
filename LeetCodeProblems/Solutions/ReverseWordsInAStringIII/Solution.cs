bffb424d2db71e81a6d7df093d53acd34e786c95
Marian Focsa <marian.focsa@kireygroup.com>
2019-08-14 11:42:25 +0300

    Added 'Reverse words in a string III' problem

---
+++ LeetCodeProblems/Solutions/ReverseWordsInAStringIII/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions.ReverseWordsInAStringIII
+{
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return s;
+
+            var sb = new StringBuilder();
+            var tempStack = new Stack<char>();
+            for (var i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                if (c != ' ')
+                    tempStack.Push(c);
+
+                if (c == ' ' || i == s.Length - 1)
+                {
+                    sb.Append(tempStack.ToArray());
+                    tempStack.Clear();
+                }
+
+                if (c == ' ')
+                    sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ LeetCodeProblems/Tests/ReverseWordsInAStringIII/ReverseWordsInAStringIIITest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.ReverseWordsInAStringIII;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.ReverseWordsInAStringIII
+{
+    [TestClass]
+    public class ReverseWordsInAStringIIITest
+    {
+        #region Setup
+        private Solution solution;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            solution = new Solution();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            solution = null;
+        }
+        #endregion
+
+        [TestMethod]
+        public void Given_Solution_When_TestingWithSampleInput_Then_Should_GetGoodResult()
+        {
+            //given = in test initialize
+            var input = "Let's take LeetCode contest";
+            var expected = "s'teL ekat edoCteeL tsetnoc";
+
+            //when - running test with sample from leetcode
+            var reverted = solution.ReverseWords(input);
+
+            //then - results should be correct
+            reverted.Should().Be(expected);
+        }
+    }
+}
